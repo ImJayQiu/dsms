c0629a528da7bdf123075f2462b9b0a9b3568d27
ImJayQiu <imjay@live.com>
2016-01-09 00:13:54 +0700

    update mult

--- app/controllers/cmip5s_controller.rb
+++ app/controllers/cmip5s_controller.rb
@@ -255,9 +255,10 @@ class Cmip5sController < ApplicationController
 
 		@sel_data_ctl = Cdo.gradsdes(input: @sel_data)
 		ntime = Cdo.ntime(input: @sel_data)[0]
+		stdname = Cdo.showstdname(input: @sel_data)[0]
 		gs_name = "lon_#{s_lon.to_i}_#{e_lon.to_i}_lat_#{s_lat.to_i}_#{e_lat.to_i}_#{@sdate.strftime('%Y%m%d')}_#{@edate.strftime('%Y%m%d')}"
 
-		grads_gs = File.new("#{sys_output_dir}/#{gs_name}.gs", "w")
+		grads_gs = File.new("#{sys_output_dir}/#{gs_name}_mean.gs", "w")
 		grads_gs.puts("reinit")
 		grads_gs.puts("open #{output_file_name}.ctl")
 		grads_gs.puts("set grads off")
@@ -266,9 +267,9 @@ class Cmip5sController < ApplicationController
 		#grads_gs.puts("set font 1")
 		#grads_gs.puts("set strsiz 0.18")
 		#grads_gs.puts("draw string 3.5 8.1 #{model} #{experiment}")
-		#grads_gs.puts("set font 2")
-		#grads_gs.puts("set strsiz 0.15")
-		#grads_gs.puts("draw string 3.5 7.8 #{@sdate} to #{@edate}")
+		grads_gs.puts("set font 1")
+		grads_gs.puts("set strsiz 0.12")
+		grads_gs.puts("draw string 4 0.2 CDAAS RIMES.INT #{Time.now.year}")
 
 		if @unit == "°C"
 			grads_gs.puts('set rgb 33 248 50 60')
@@ -346,14 +347,111 @@ class Cmip5sController < ApplicationController
 
 		grads_gs.puts("d ave(#{var}*#{@rate}+#{@rate2},t=1,t=#{ntime.to_s})")
 		#grads_gs.puts('./public/cbar.gs')
-		grads_gs.puts("draw title #{model} #{experiment} #{mip} #{var} ")
-		grads_gs.puts("printim #{output_file_name}_sel_lonlat_grads.png png white")
+		grads_gs.puts("draw title #{model} Daily #{experiment.humanize} #{stdname.humanize} | Mean")
+		grads_gs.puts("printim #{output_file_name}_sel_lonlat_grads_mean.png png white")
+		grads_gs.puts("quit")
+		grads_gs.close
+
+
+		grads_gs = File.new("#{sys_output_dir}/#{gs_name}_max.gs", "w")
+		grads_gs.puts("reinit")
+		grads_gs.puts("open #{output_file_name}.ctl")
+		grads_gs.puts("set grads off")
+		grads_gs.puts("set gxout shaded")
+		grads_gs.puts("set mpdset hires")
+		grads_gs.puts("set font 1")
+		#grads_gs.puts("set strsiz 0.18")
+		#grads_gs.puts("draw string 3.5 8.1 #{model} #{experiment}")
+		#grads_gs.puts("set font 2")
+		grads_gs.puts("set strsiz 0.12")
+		grads_gs.puts("draw string 4 0.2 CDAAS RIMES.INT #{Time.now.year}")
+
+		if @unit == "°C"
+			grads_gs.puts('set rgb 33 248 50 60')
+			grads_gs.puts('set rgb 34 255 50 89')
+			grads_gs.puts('set rgb 35 255 50 185')
+			grads_gs.puts('set rgb 36 248 50 255')
+			grads_gs.puts('set rgb 37 224 50 255')
+			grads_gs.puts('set rgb 38 195 50 255')
+			grads_gs.puts('set rgb 39 175 50 255')
+			grads_gs.puts('set rgb 40 161 50 255')
+			grads_gs.puts('set rgb 41 137 50 255')
+			grads_gs.puts('set rgb 42 118 74 255')
+			grads_gs.puts('set rgb 43 98 74 255')
+			grads_gs.puts('set rgb 44 79 50 255')
+			grads_gs.puts('set rgb 45 50 50 255')
+			grads_gs.puts('set rgb 46 50 74 255')
+			grads_gs.puts('set rgb 47 50 89 255')
+			grads_gs.puts('set rgb 48 50 113 255')
+			grads_gs.puts('set rgb 49 50 146 255')
+			grads_gs.puts('set rgb 50 50 175 255')
+			grads_gs.puts('set rgb 51 50 204 255')
+			grads_gs.puts('set rgb 52 50 224 255')
+			grads_gs.puts('set rgb 53 50 255 253')
+			grads_gs.puts('set rgb 54 50 255 228')
+			grads_gs.puts('set rgb 55 50 255 200')
+			grads_gs.puts('set rgb 56 50 255 161')
+			grads_gs.puts('set rgb 57 50 255 132')
+			grads_gs.puts('set rgb 58 50 255 103')
+			grads_gs.puts('set rgb 59 50 255 79')
+			grads_gs.puts('set rgb 60 50 255 60')
+			grads_gs.puts('set rgb 61 79 255 50')
+			grads_gs.puts('set rgb 62 132 255 50')
+			grads_gs.puts('set rgb 63 171 255 50')
+			grads_gs.puts('set rgb 64 204 255 50')
+			grads_gs.puts('set rgb 65 224 255 50')
+			grads_gs.puts('set rgb 66 253 255 50')
+			grads_gs.puts('set rgb 67 255 233 50')
+			grads_gs.puts('set rgb 68 255 224 50')
+			grads_gs.puts('set rgb 69 255 209 50')
+			grads_gs.puts('set rgb 70 255 204 50')
+			grads_gs.puts('set rgb 71 255 185 50')
+			grads_gs.puts('set rgb 72 255 161 50')
+			grads_gs.puts('set rgb 73 255 146 50')
+			grads_gs.puts('set rgb 74 255 127 50')
+			grads_gs.puts('set rgb 75 255 118 50')
+			grads_gs.puts('set rgb 76 255 93 50')
+			grads_gs.puts('set rgb 77 255 74 50')
+			grads_gs.puts('set rgb 78 255 60 50')
+			grads_gs.puts('set rgb 79 255 33 33')
+			grads_gs.puts('set rgb 80 255 0 0')
+			grads_gs.puts('set rgb 81 235 10 0')
+			grads_gs.puts('set rgb 82 215 20 0')
+			grads_gs.puts('set rgb 83 195 30 0')
+			grads_gs.puts('set rgb 84 175 40 0')
+			grads_gs.puts('set rgb 85 165 45 0')
+			grads_gs.puts('set rgb 86 155 50 0')
+			grads_gs.puts('set rgb 87 145 55 0')
+			grads_gs.puts('set rgb 88 135 60 0')
+			grads_gs.puts('set rgb 89 120 60 0')
+			grads_gs.puts('set rgb 90 100 60 0')
+			grads_gs.puts('set rgb 91 80 60 0')
+			grads_gs.puts('set rgb 20 250 240 230')
+			grads_gs.puts('set rgb 21 240 220 210')
+			grads_gs.puts('set rgb 22 225 190 180')
+			grads_gs.puts('set rgb 23 200 160 150')
+			grads_gs.puts('set rgb 24 180 140 130')
+			grads_gs.puts('set rgb 25 160 120 110')
+			grads_gs.puts('set rgb 26 140 100 90')
+			grads_gs.puts("set clevs -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 ")
+			grads_gs.puts('set ccols 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 85 87 88 89 90 91 26 25 24 23 22 21 20')
+		elsif @unit == "mm/d"
+			grads_gs.puts("set clevs 0 2 4 6 8 10 12 14 16 18 20 22")
+			grads_gs.puts('set ccols 0 13 3 10 7 12 8 2 6 14 4')
+		end
+
+		grads_gs.puts("d max(#{var}*#{@rate}+#{@rate2},t=1,t=#{ntime.to_s})")
+		#grads_gs.puts('./public/cbar.gs')
+		grads_gs.puts("draw title #{model} Daily #{experiment.humanize} #{stdname.humanize} | Max")
+		grads_gs.puts("printim #{output_file_name}_sel_lonlat_grads_max.png png white")
 		grads_gs.puts("quit")
 		grads_gs.close
 
 		@go_dir = "cd #{sys_output_dir.to_s}"
-		@plot_cmd = "grads -lbc 'exec #{gs_name}.gs'"
-		@plot = system("cd / && #{@go_dir} && #{@plot_cmd} ") 
+		@plot_mean = "grads -lbc 'exec #{gs_name}_mean.gs'"
+		@plot_max = "grads -lbc 'exec #{gs_name}_max.gs'"
+		@plot_mean_cmd = system("cd / && #{@go_dir} && #{@plot_mean} ") 
+		@plot_max_cmd = system("cd / && #{@go_dir} && #{@plot_max} ") 
 	end
 
 
@@ -646,7 +744,7 @@ class Cmip5sController < ApplicationController
 
 		#	@f1_data_ctl = Cdo.gradsdes(input: @f1_data)
 		#	@f2_data_ctl = Cdo.gradsdes(input: @f2_data)
-		@f3_data_ctl = Cdo.gradsdes(input: @f3_data)
+		#   @f3_data_ctl = Cdo.gradsdes(input: @f3_data)
 		#	@f4_data_ctl = Cdo.gradsdes(input: @f4_data)
 		##############################################################
 		gs_name = "lon_#{s_lon.to_i}_#{e_lon.to_i}_lat_#{s_lat.to_i}_#{e_lat.to_i}_#{@sdate.strftime('%Y%m%d')}_#{@edate.strftime('%Y%m%d')}"
@@ -662,6 +760,7 @@ class Cmip5sController < ApplicationController
 				m_name = m4
 			end
 			ntime = Cdo.ntime(input: data)[0] rescue nil
+			stdname = Cdo.showstdname(input: data)[0] rescue nil
 			data_ctl = Cdo.gradsdes(input: data) rescue nil
 			gs_name_m = "lon_#{s_lon.to_i}_#{e_lon.to_i}_lat_#{s_lat.to_i}_#{e_lat.to_i}_#{@sdate.strftime('%Y%m%d')}_#{@edate.strftime('%Y%m%d')}_#{i+1}"
 
@@ -670,11 +769,11 @@ class Cmip5sController < ApplicationController
 			grads_gs.puts("open #{output_file_name}_#{m_name}.ctl")
 			grads_gs.puts("set grads off")
 			grads_gs.puts("set gxout shaded")
-			grads_gs.puts("set font 2")
-			grads_gs.puts("set strsiz 0.12")
-			grads_gs.puts("draw string 3.5 7.8 #{@sdate} to #{@edate}")
-			grads_gs.puts("set font 1")
-			grads_gs.puts("set mpdset hires")
+			#	grads_gs.puts("set font 2")
+			#	grads_gs.puts("set strsiz 0.12")
+			#	grads_gs.puts("draw string 3.5 7.8 #{@sdate} to #{@edate}")
+			#	grads_gs.puts("set font 1")
+			#	grads_gs.puts("set mpdset hires")
 
 			if @unit == "°C"
 				grads_gs.puts('set rgb 33 248 50 60')
@@ -751,8 +850,8 @@ class Cmip5sController < ApplicationController
 			end
 			grads_gs.puts("d ave(#{var}*#{@rate}+#{@rate2},t=1,t=#{ntime.to_s})")
 			#grads_gs.puts('/public/cbar.gs')
-			grads_gs.puts("draw title #{m_name} #{exp} ")
-			grads_gs.puts("printim #{output_file_name}_sel_lonlat_grads_#{i+1}.png png white")
+			grads_gs.puts("draw title #{m_name} Daily #{exp.humanize} #{stdname.humanize}") rescue nil
+			grads_gs.puts("printim #{output_file_name}_sel_lonlat_grads_#{i+1}.png png white") rescue nil
 			grads_gs.puts("quit")
 			grads_gs.close
 		end
