d5e7256b957e0cba5585d4fd7c12b92cb6d92573
ImJayQiu <imjay@live.com>
2017-02-14 15:38:36 +0700

    csv update

--- app/controllers/ecmwf_controller.rb
+++ app/controllers/ecmwf_controller.rb
@@ -32,9 +32,9 @@ class EcmwfController < ApplicationController
 		############# paramaters  ################################
 		@var = var = params[:part1].first.to_s
 		mip = 'ECMWF' 
-		#model = params[:part3].first.to_s
 		@type = type = params[:part4].first.to_s
 
+		@output_name = @date.strftime("%Y_%m_%d") + '_' + mip + '_' + type + '_' + var + '_' + lon_lat 
 		###########################################
 
 		### related file names
@@ -47,7 +47,7 @@ class EcmwfController < ApplicationController
 		###########################################
 
 		### output folder and files name setting
-		output_dir = "tmp_nc/#{current_user.id}/#{mip}/#{date_folder}/#{type}/#{var}/#{lon_lat}"
+		output_dir = "tmp_nc/#{current_user.id}/#{mip}/#{date_folder}/#{type}/#{var}/#{lon_lat}/"
 		sys_output_pub = Rails.root.join("public")
 		@sys_output_dir = Rails.root.join("public", output_dir)
 
@@ -59,9 +59,9 @@ class EcmwfController < ApplicationController
 
 		### cut data by selected lat lon 
 
-		@cdo_output_path = output_dir.to_s + "/" + lon_lat 
+		#@cdo_output_path = output_dir.to_s + "/" + lon_lat 
 
-		@sel_data = cdo_run.sellonlatbox([s_lon,e_lon,s_lat,e_lat], input: data_path, output: "public/#{@cdo_output_path}.nc")
+		@sel_data = cdo_run.sellonlatbox([s_lon,e_lon,s_lat,e_lat], input: data_path, output: "#{@sys_output_dir + @output_name}.nc")
 		#########################################
 
 		@data = cdo_run.sinfon(input: @sel_data)
@@ -71,9 +71,9 @@ class EcmwfController < ApplicationController
 		@ctl_file = cdo_run.gradsdes(input: @sel_data)
 		#############################################
 
-		grads_gs = File.new("public/#{@cdo_output_path}.gs", "w")
+		grads_gs = File.new("#{@sys_output_dir + lon_lat}.gs", "w")
 		grads_gs.puts("reinit")
-		grads_gs.puts("open #{lon_lat}.ctl")
+		grads_gs.puts("open #{@output_name}.ctl")
 		grads_gs.puts("draw title ECMWF #{type} #{var}")
 		grads_gs.puts("set gxout shaded")
 		grads_gs.puts("set mpdset hires")
@@ -82,7 +82,7 @@ class EcmwfController < ApplicationController
 			grads_gs.puts("draw string 2.8 0.2 Forcasting Date: #{t} By CDAAS RIMES.INT #{Time.now.year}")
 			grads_gs.puts("set t #{i+1} " )
 			grads_gs.puts("d #{var}" )
-			grads_gs.puts("printim #{lon_lat}-#{i+1}.png white")
+			grads_gs.puts("printim #{@output_name}-#{i+1}.png white")
 			grads_gs.puts("clear")
 			grads_gs.puts("set grads off")
 		end
@@ -92,7 +92,8 @@ class EcmwfController < ApplicationController
 		@go_dir = "cd #{@sys_output_dir.to_s}"
 		@plot_maps = "grads -lbc 'exec #{lon_lat}.gs'"
 		@plot_maps_cmd = system("cd / && #{@go_dir} && #{@plot_maps} ") 
-		@ani_maps_cmd = system("cd / && #{@go_dir} && convert -delay 100 '#{lon_lat}-%d.png[0-999]' #{lon_lat}_all.gif ") 
+		@print_csv_cmd = system("cd / && #{@go_dir} && cdo outputtab,date,time,lon,lat,value #{@sel_data} > #{@output_name}.csv ") 
+		@ani_maps_cmd = system("cd / && #{@go_dir} && convert -delay 100 '#{@output_name}-%d.png[0-999]' #{@output_name}_all.gif ") 
 
 
 	end
